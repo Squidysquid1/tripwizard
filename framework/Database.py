764873e76293caeee6c882fd90cda54a88427e20
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2024-10-14 11:09:45 -0500

    add querying to database

--- framework/Database.py
+++ framework/Database.py
@@ -10,3 +10,9 @@ class Database:
     def __init__(self,app):
         self.connector = sqlite3.connect(config.DATABASE_NAME)
         self.cursor = self.connector.cursor()
+
+    def query(self, sql):
+        return self.cursor.execute(sql)
+    
+    def prepared_query(self, sql, params):
+        return self.cursor.execute(sql, params)
